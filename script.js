f69baab678fd73c71263f5d0bc7512615b03837e
RockVi <107112402+RockVi@users.noreply.github.com>
2023-06-21 13:16:17 +0200

    Particles

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var particlesContainer = document.getElementById('particlesContainer');
-  
+    let particlesContainer = document.getElementById('particlesContainer');
+
     particlesContainer.addEventListener('mousemove', function(event) {
-      var particle = document.createElement('div');
+      let particle = document.createElement('div');
       particle.className = 'particle';
-      particle.style.top = event.clientY + 'px';
+      particle.style.top = event.pageY + 'px';
       particle.style.left = event.clientX + 'px';
-  
+
       particlesContainer.appendChild(particle);
   
       setTimeout(function() {
         particle.style.opacity = 1;
-      }, 0);
-  
+      }, 1);
+      
       setTimeout(function() {
         particlesContainer.removeChild(particle);
-      }, 2000);
+      }, 1000);
     });
   });
   
